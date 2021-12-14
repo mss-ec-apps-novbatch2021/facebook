c0ab7827525fac858bce61c94d84866ad2fa9cdd
VechamAkhil <vecham1999@gmail.com>
2021-12-14 17:25:28 +0530

    First commit

---
+++ dbut.java
@@ -0,0 +1,1 @@
+hello good morning now we are going to prACTISE GIT COMMANDS
---
+++ string.java
@@ -0,0 +1,3 @@
+this is about strings in git commands
+strings are the naming variables
+in java we can append the strings
